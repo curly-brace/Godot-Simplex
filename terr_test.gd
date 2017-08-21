7cec0989e9a28807995f21ab034297a3f011b74e
Curly Brace <stasuss@gmail.com>
2017-08-21 13:39:22 +0300

    fixed sampling artifacts by setting viewport to 3d mode
    seems like thins is another bug

--- terr_test.gd
+++ terr_test.gd
@@ -5,10 +5,11 @@ onready var vp = get_node('vp')
 onready var simplex = get_node('vp/simplex')
 onready var spr = get_node('test')
 var size = 64
-var scale = 2.0
-var cur_offset = Vector2(0, 0)
+var scale = 4.0
+var cur_offset = Vector2(5, 5)
 var update_interval = 0.05
 var timer = 0.0
+var updating = true
 
 func _ready():
     var t = ImageTexture.new()
@@ -17,28 +18,34 @@ func _ready():
     vp.size = Vector2(size, size)
 
     simplex.get_material().set_shader_param('scale', scale)
+    simplex.get_material().set_shader_param('offset', cur_offset)
     mi.get_multimesh().instance_count = size * size
 
 func _process(delta):
     timer += delta
     cur_offset += Vector2(delta, delta)
-    if timer >= update_interval:
+    if updating and timer >= update_interval:
         timer = 0.0
-
-        var mm = mi.get_multimesh()
-
-        var t = Transform()
-        #it is supposed to be direct viewporttexture, but it fails because of bugs
-        var s = spr.get_texture().get_data()
-        s.lock()
-        var p
-        for x in range(size):
-            for y in range(size):
-                p = s.get_pixel(x, y).r
-                mm.set_instance_transform(x*size+y, t.translated(Vector3(x, p, y)))
-        s.unlock()
+        update_field()
         simplex.get_material().set_shader_param('offset', cur_offset)
+        #for single update
+        #updating = false
 
+func update_field():
+    var mm = mi.get_multimesh()
+
+    var t = Transform()
+    #it is supposed to be direct viewporttexture, but it fails because of bugs
+    #setting viewport to 2d mode shows some sort of artifacts on the values
+    #but sprite's texture looks good
+    var s = spr.get_texture().get_data()
+    s.lock()
+    var p
+    for x in range(size):
+        for y in range(size):
+            p = s.get_pixel(x, y).r
+            mm.set_instance_transform(x*size+y, t.translated(Vector3(x, (p+p)*2.0, y)))
+    s.unlock()
 
 
 
